e58ca9826fd00734709b5acf928cc9eaa600a713
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-10 16:17:38 -0800

    tested avg pair

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,27 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
-}
+/**
+ * Input an sorted array of numbers and a target number,
+ * return true or false based on whether there is a pair of numbers in the array
+ * where the average of the two numbers is the target number
+ */
 
+function averagePair(sortedNums, targetAvg) {
+  let leftIndex = 0;
+  let rightIndex = sortedNums.length - 1;
+
+  // use multiple pointers to maintain linear complexity
+  while (leftIndex < rightIndex) {
+    const iterationAvg = (sortedNums[leftIndex] + sortedNums[rightIndex]) / 2;
+
+    if (iterationAvg === targetAvg) {
+      return true;
+    }
+    else if (iterationAvg < targetAvg) {
+      leftIndex++;
+    }
+    else {
+      rightIndex--;
+    }
+  }
+
+  return false;
+}
