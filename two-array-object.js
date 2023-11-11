8dbb9c452d3191ce83f299ca940ff573908cb96e
jswwWorks <jsww.works@gmail.com>
2023-11-10 19:44:18 -0500

    Completed twoArrayObject problem.

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** Takes two arrays, one named keys and the other named values.
+ * Assembles an object using keys as keys and values and values.
+ * Returns object.
+ */
+
+function twoArrayObject(keys, values) {
+  let outputObject = {};
+
+  for (let i = 0; i < keys.length; i++) {
+
+    // handles cases where values array is less than keys array
+    if (i > (values.length - 1)) {
+      values.push(null);
+    }
+
+    outputObject[keys[i]] = values[i];
+  }
+
+  return outputObject;
 }
